fc78200a30a325c69ba5dffe39c07cd08099ad25
becomunica <dalayon@becomunica.com>
2021-09-22 23:35:39 +0200

    setup storybook

---
+++ src/stories/Button.stories.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { withDesign } from 'storybook-addon-designs'
+
+import { Button } from './Button';
+
+export default {
+  title: 'Example/Button',
+  parameters: {
+    assets: [
+      'https://www.figma.com/file/jOxUmFHa6YS2ezrztJmdJs/Sort-column-in-preview-mode?node-id=0%3A1', // link to a webpage
+    ],
+  },
+  component: Button,
+  decorators: [withDesign],
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
+};
